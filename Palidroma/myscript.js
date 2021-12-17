d484a99871fd98d63a61a2998731fc23dc42131b
ValleLuca <valle.lucaAb@outlook.it>
2021-12-17 16:31:53 +0100

    Blocco controllo

--- Palidroma/myscript.js
+++ Palidroma/myscript.js
@@ -4,22 +4,21 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
-function reverseFunction(valore){
-    for(let i = 0; i < valore.lenght; i++) {
-        console.log(valore);
+function reverseFunction(valore){ //creare un funzione
+    let confrontoArr = [];
+    for(let i = 0; i < valore.length; i++) { //controllare con un'array se la parola al contrairo è uguale
+        
+        confrontoArr.push(valore[i]);
+
+        
     }
+    console.log(confrontoArr);
 }
 
-let parolaUtente = prompt("Inserire la parola palidroma");
-console.log(parolaUtente[1]);
-console.log(parolaUtente[2]);
-console.log(parolaUtente[3]);
-console.log(parolaUtente[4]);
-console.log(parolaUtente.lenght);
-
+let parolaUtente = prompt("Inserire la parola palidroma"); //chiedere la parola
 let parola = reverseFunction(parolaUtente);
 
-//chiedere la parola
-//creare un funzione
-//controllare con un'array se la parola al contrairo è uguale
+
+
+
 //stampare un risultato
