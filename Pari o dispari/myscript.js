79a3e70f1422da16d1a982acacacb442ce1baff0
ValleLuca <valle.lucaAb@outlook.it>
2021-12-17 18:37:43 +0100

    Pari o dispari, manca controllo utente vs pc

--- Pari o dispari/myscript.js
+++ Pari o dispari/myscript.js
@@ -7,9 +7,46 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
 
-//creare una select in html per scegliere pari o dispari
-//creare un bottone che fara partire un prompt
-let numeroUtente = parseInt(prompt("inserisci il numero")); //creare un prompt per inserire il numero
-//creare una funzione per generare un numero random per il rivale "pc"
-//sommare i due numeri
-//creare una funzione per controllare se i due numeri sommati sono pari o dispati
+function generatoreRandom(dUtente){                     //creare una funzione per generare un numero random per il rivale "pc"
+
+    let dPc = Math.floor(Math.random() * 5 + 1);        //creazione dato random da 1 a 5
+    let somma = dUtente + dPc;                          //sommare i due numeri
+    return somma;
+
+}
+
+function checkPariDispari(pariOdispari){                     //creare una funzione per controllare se il risultato è pari o dispari
+let finale = "";
+    if(pariOdispari % 2 == 0){
+        finale = "pari";
+    }
+    else
+    {
+        finale = "dispari";
+    }
+
+    return finale;
+}
+
+function who(vincitore){
+
+    let finalista = "Ha vinto il pc";
+    
+    if(vincitore == pariodispariUtente){
+        finalista = "Ha vinto l'utente"
+    }
+
+    return finalista;
+
+} 
+
+let numeroUtente = parseInt(prompt("inserisci il numero da 1 a 5"));    //creare un prompt per inserire il numero
+let datoUtenteFunction = generatoreRandom(numeroUtente);                //assegnazione dato utente alla function
+console.log(datoUtenteFunction);                                        //Risultato somma in console
+
+let check = checkPariDispari(datoUtenteFunction);                       //assegnazione somma alla function
+console.log(check);                                                     //Risultato pari o dispari
+
+let chihavinto = who(check);                                           //assegnazione risultato pari o dispari alla function
+let pariodispariUtente = prompt("inserisci pari o dispari");            //creare un prompt per inserire pari o dispari
+console.log(chihavinto);                                               //Risultato pari o dispari
