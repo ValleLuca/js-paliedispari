60754d8f960c0b2603ebc6ab6af94f85cc30c95c
ValleLuca <valle.lucaAb@outlook.it>
2021-12-17 15:34:37 +0100

    Aggiunte variabili e inizio function

--- Palidroma/myscript.js
+++ Palidroma/myscript.js
@@ -4,6 +4,15 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
 
+function reverseFunction(valore){
+    if(let i = 0; i < valore.lenght; i++) {
+        console.log(valore);
+    }
+}
+
+let parolaUtente = prompt("Inserire la parola palidroma");
+let parola = reverseFunction(parolaUtente);
+
 //chiedere la parola
 //creare un funzione
 //controllare con un'array se la parola al contrairo è uguale
