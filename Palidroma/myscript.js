9d30aa8030a4913141d40fa24048dbeee408549f
ValleLuca <valle.lucaAb@outlook.it>
2021-12-17 13:03:31 +0100

    Aggiunti commenti per iniziare

---
+++ Palidroma/myscript.js
@@ -0,0 +1,10 @@
+/*
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+//chiedere la parola
+//creare un funzione
+//controllare con un'array se la parola al contrairo è uguale
+//stampare un risultato
---
+++ Pari o dispari/myscript.js
@@ -0,0 +1,15 @@
+/*
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+//creare una select in html per scegliere pari o dispari
+//creare un bottone che fara partire un prompt
+//creare un prompt per inserire il numero
+//creare una funzione per generare un numero random per il rivale "pc"
+//sommare i due numeri
+//creare una funzione per controllare se i due numeri sommati sono pari o dispati
